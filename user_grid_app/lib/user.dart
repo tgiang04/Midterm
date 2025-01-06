40e8ef0c502cf78cc9e4dca7b478654f742d8aa8
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2025-01-06 15:24:15 +0700

    Update user.dart

--- user_grid_app/lib/user.dart
+++ user_grid_app/lib/user.dart
@@ -3,5 +3,11 @@ class User {
   String password;
   String role;
 
+  // Constructor
   User({required this.username, required this.password, required this.role});
+
+  // Hàm để hiển thị thông tin người dùng
+  void displayInfo() {
+    print('Username: $username, Role: $role');
+  }
 }
