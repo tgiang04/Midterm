13bf19630772029f8522dd3e8f29f63525937c6b
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2025-01-06 06:32:46 +0000

    Create User's data

---
+++ user_grid_app/lib/user_data.dart
@@ -0,0 +1,9 @@
+import 'user.dart';
+
+List<User> users = [
+  User(username: 'user1', password: 'pass1', role: 'admin'),
+  User(username: 'user2', password: 'pass2', role: 'editor'),
+  User(username: 'user3', password: 'pass3', role: 'viewer'),
+  User(username: 'user4', password: 'pass4', role: 'contributor'),
+  User(username: 'user5', password: 'pass5', role: 'guest'),
+];
